9cbc81230d68c89fa032da5008c69f64672fcd72
Theo Brigitte <theo.brigitte@gmail.com>
2020-01-28 11:23:07 +0100

    add comments and fix error when not matching patch files

--- script/sync_chart.sh
+++ script/sync_chart.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Exit on error
 set -e
+
+# Expand filename pattern to null when no files are found
+shopt -s nullglob
 
 UPSTREAM_REPO_URL="https://github.com/open-policy-agent/gatekeeper.git"
 UPSTREAM_CHART_PATH="chart/gatekeeper-operator/"
 
 LOCAL_CHART_PATH="../helm/gatekeeper-app/"
-PATCH_FILES="../patch/*.patch"
+PATCH_FILES="$SCRIPT_PATH/../patch/*.patch"
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 TMP=$(mktemp -d)
@@ -19,6 +23,6 @@ echo "=====> syncing chart"
 rsync -av --delete --exclude-from=$SCRIPT_PATH/ignored_files $TMP/$UPSTREAM_CHART_PATH $SCRIPT_PATH/$LOCAL_CHART_PATH
 
 echo "=====> patching chart"
-for file in $SCRIPT_PATH/$PATCH_FILES; do
+for file in $PATCH_FILES; do
     patch -d helm/gatekeeper-app/templates < $file
 done
