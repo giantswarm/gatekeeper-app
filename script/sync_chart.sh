8954f074370ad3e495883b8580b6b22ebc13fb22
Theo Brigitte <theo.brigitte@gmail.com>
2020-01-28 15:21:15 +0100

    make all variables lowercase

--- script/sync_chart.sh
+++ script/sync_chart.sh
@@ -4,37 +4,38 @@
 # It does so by pulling the upstream chart and apply patches on top
 
 # Gatekeeper upstream git repository url
-UPSTREAM_REPO_URL="https://github.com/open-policy-agent/gatekeeper.git"
+upstream_repo_url="https://github.com/open-policy-agent/gatekeeper.git"
+
 # Path to helm chart in the gatekeeper upstream repository
-UPSTREAM_CHART_PATH="chart/gatekeeper-operator/"
+upstream_chart_path="chart/gatekeeper-operator/"
 
 # Path to helm chart in this repository, relative to this script
-LOCAL_CHART_PATH="../helm/gatekeeper-app/"
+local_chart_path="../helm/gatekeeper-app/"
 # Path to patch files in this repository, relative to this script
-PATCH_FILES="../patch/*.patch"
+patch_files="../patch/*.patch"
 
 # Path to ignored files list, relative to this script
-IGNORED_FILES="ignored_files"
+ignored_files="ignored_files"
 
 # Exit on error
 set -e
 # Expand filename pattern to null when no files are found
 shopt -s nullglob
 
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
-TMP=$(mktemp -d)
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+tmp=$(mktemp -d)
 # Clean temporary directory on exit
-trap "rm -rf $TMP" EXIT
+trap "rm -rf $tmp" EXIT
 
 echo "=====> fetching upstream chart"
-git clone $UPSTREAM_REPO_URL $TMP
+git clone $upstream_repo_url $tmp
 
 echo "=====> syncing chart"
-rsync -av --delete --exclude-from=$SCRIPT_PATH/$IGNORED_FILES /$TMP/$UPSTREAM_CHART_PATH $SCRIPT_PATH/$LOCAL_CHART_PATH
+rsync -av --delete --exclude-from=$script_path/$ignored_files $tmp/$upstream_chart_path $script_path/$local_chart_path
 
 echo "=====> patching chart"
-for file in $SCRIPT_PATH/$PATCH_FILES; do
-    patch -p1 -d $SCRIPT_PATH/$LOCAL_CHART_PATH < $file
+for file in $script_path/$patch_files; do
+    patch -p1 -d $script_path/$local_chart_path < $file
 done
 
 echo "=====> done"
