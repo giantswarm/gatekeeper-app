3b033c20131928681e7e20bd17ef7f525dda009e
Theo Brigitte <theo.brigitte@gmail.com>
2020-01-28 15:07:04 +0100

    more comments and variables

--- script/sync_chart.sh
+++ script/sync_chart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script generate the gatekeeper helm chart
-# It does so by pulling the upstream chart and apply patches on top.
+# It does so by pulling the upstream chart and apply patches on top
 
 # Gatekeeper upstream git repository url
 UPSTREAM_REPO_URL="https://github.com/open-policy-agent/gatekeeper.git"
@@ -13,6 +13,9 @@ LOCAL_CHART_PATH="../helm/gatekeeper-app/"
 # Path to patch files in this repository, relative to this script
 PATCH_FILES="../patch/*.patch"
 
+# Path to ignored files list, relative to this script
+IGNORED_FILES="ignored_files"
+
 # Exit on error
 set -e
 # Expand filename pattern to null when no files are found
@@ -20,13 +23,14 @@ shopt -s nullglob
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 TMP=$(mktemp -d)
+# Clean temporary directory on exit
 trap "rm -rf $TMP" EXIT
 
 echo "=====> fetching upstream chart"
 git clone $UPSTREAM_REPO_URL $TMP
 
 echo "=====> syncing chart"
-rsync -av --delete --exclude-from=$SCRIPT_PATH/ignored_files $TMP/$UPSTREAM_CHART_PATH $SCRIPT_PATH/$LOCAL_CHART_PATH
+rsync -av --delete --exclude-from=$SCRIPT_PATH/$IGNORED_FILES /$TMP/$UPSTREAM_CHART_PATH $SCRIPT_PATH/$LOCAL_CHART_PATH
 
 echo "=====> patching chart"
 for file in $SCRIPT_PATH/$PATCH_FILES; do
